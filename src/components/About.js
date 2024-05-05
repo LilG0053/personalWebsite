a8c892fd21dae1a1808b9e8072f4354363dc6855
LilG0053 <ekimmel8@gatech.edu>
2024-05-05 15:41:07 -0400

    more formatting testing

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,11 @@ export default function About() {
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                     Hi, I'm Ethan
-                    <br className="hidden xl:inline-block" />I love immersive experiences.
+                    <h2 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                    I love immersive experiences.
+                    </h2>
                 </h1>
+                
                 <p className="mb-8 leading-relaxed">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
                     laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
