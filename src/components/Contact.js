64baa8ff2035462ce86a2b1f92f6e484b81e96d0
LilG0053 <ekimmel8@gatech.edu>
2024-05-06 00:19:48 -0400

    contact reformatting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,15 @@
 import React from "react";
 
 export default function Contact() {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [message, setMessage] = React.useState("");
-
-    function encode(data) {
-        return Object.keys(data)
-            .map(
-                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-            )
-            .join("&");
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        fetch("/", {
-            method: "POST",
-            headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: encode({ "form-name": "contact", name, email, message }),
-        })
-        .then(() => alert("Message sent!"))
-        .catch((error) => alert(error));
-    }
     return (
-    <section id="contact" className="relative">
-        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                    <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+    <section id="contact" className="text-center">
+        <h1 className = "sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                Contact Me
+        </h1>
+        <div className="container py-0 mx-auto justify-center flex sm:flex-nowrap flex-wrap">
+            <div className="bg-gray-900 rounded-lg overflow-hidden p-5 flex relative">
+                <div className="bg-gray-800 relative flex py-5 rounded shadow-md">
+                    <div className="px-6 lg:mt-0">
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                             EMAIL
                         </h2>
@@ -38,65 +19,34 @@ export default function Contact() {
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                             PHONE
                         </h2>
-                        <p className="leading-relaxed">914-705-3328</p>
+                        <p className="sm:text-ellipsis leading-relaxed">914-705-3328</p>
                     </div>
                 </div>
+                {/* flex row to position the icons to the right */}
             </div>
-            <form
-            netlify
-            name="contact"
-            onSubmit={handleSubmit}
-            className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                    Hire Me
-                </h2>
-                <p className="leading-relaxed mb-5">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                    suscipit officia aspernatur veritatis. Asperiores, aliquid?
-                </p>
-                <div className="relative mb-4">
-                    <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                        Name
-                    </label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        onChange ={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                    Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="relative mb-4">
-                    <label
-                    htmlFor="message"
-                    className="leading-7 text-sm text-gray-400">
-                    Message
-                    </label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                        onChange={(e) => setMessage(e.target.value)}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Submit
-                </button>
-            </form>
+        </div>
+        <div className="flex flex-row justify-center pt-3 pb-1">
+            <a href="https://github.com/LilG0053">
+                <img className="rounded px-1"
+                    alt="hero"
+                    src="./github.png"
+                    width="60"
+                    height="60" />
+            </a>
+            <a href="https://www.linkedin.com/in/ethan-kimmel-25a065262">
+                <img className="rounded px-1"
+                    alt="hero"
+                    src="./linkedin.png"
+                    width="60"
+                    height="60" />
+            </a>
+            <a href="mailto:ekimmel8@gatech.edu">
+                <img className="rounded px-1"
+                    alt="hero"
+                    src="./email.png"
+                    width="60"
+                    height="60" />
+            </a>
         </div>
     </section>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
             <div className="text-center mb-20">
                 <div className="text-center mb-20">
                     <ChipIcon className="w-10 incline-block mb-4" />
-                    <h1 className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                    <h1 className="sm:text-4xl text-3xl font-medium justify-center title-font mb-4 text-white">
                         Skills &amp; Technologies
                     </h1>
                     <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
